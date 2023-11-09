adfc977c93376b658e6ad959ba897f3f21818b5e
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-11-09 12:23:16 -0300

    chore(vscode): remove unused status bar color customizations chore(vscode): remove unused terminal background color customization chore(vscode): remove unused terminal debugging background color customization chore(vscode): remove unused activity bar background color customization chore(vscode): remove unused title bar active background color customization chore(vscode): remove unused title bar active foreground color customization chore(app.component.html): remove unused h1 element feat(app.component.html): add app-searchbar component to main section of app component template chore(app.component.ts): remove unused imports feat(app.component.ts): add isDark property and toggleTheme method to app component to enable dark mode feat(base.scss): add base styles for html and body elements chore(main.scss): remove unused imports feat(main.scss): import base styles and utilities variables and mixins feat(mixins.scss): add media-query mixin to handle media queries chore(variables.scss): remove unused imports feat(variables.scss): add media query for desktop breakpoints to adjust font sizes and line heights chore(main.server.ts): remove unused import statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,29 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { DefinitionCardComponent } from './components/card/definition-card/definition-card.component';
+import { SearchbarComponent } from './components/forms/searchbar/searchbar.component';
 import { MainNavComponent } from './components/navigation/main-nav/main-nav.component';
+import { WordDisplayComponent } from './components/word-display/word-display.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainNavComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MainNavComponent,
+    SearchbarComponent,
+    WordDisplayComponent,
+    DefinitionCardComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'dictionary-angular-scss-app';
+  isDark = false;
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+  }
 }
