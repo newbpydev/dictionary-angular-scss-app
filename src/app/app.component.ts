3c8ef70f482d9f295849c685f915d97dc5799a92
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-11-10 08:36:49 -0300

    chore(angular.json): update stylePreprocessorOptions to include the "./src/assets/styles" directory for scss imports chore(app.component.html): restructure main container to include word display, definition card, and footer components chore(app.component.ts): add OnInit interface, update template and styles to use inline template and styles, add toggleTheme method and updateBodyClass method to handle dark mode chore(base.scss): update body styles to include height, width, and overflow properties, add dark mode styles to body and container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { DefinitionCardComponent } from './components/card/definition-card/definition-card.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { SearchbarComponent } from './components/forms/searchbar/searchbar.component';
 import { MainNavComponent } from './components/navigation/main-nav/main-nav.component';
 import { WordDisplayComponent } from './components/word-display/word-display.component';
@@ -17,14 +18,61 @@ import { WordDisplayComponent } from './components/word-display/word-display.com
     SearchbarComponent,
     WordDisplayComponent,
     DefinitionCardComponent,
+    FooterComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  // templateUrl: './app.component.html',
+  template: /*html*/ `
+    <main class="main container" [ngClass]="isDark ? 'dark' : ''">
+      <app-main-nav />
+
+      <app-searchbar />
+
+      <app-word-display />
+
+      <app-definition-card />
+
+      <app-footer />
+
+      <router-outlet></router-outlet>
+    </main>
+  `,
+  // styleUrl: './app.component.scss',
+  styles: /*scss*/ `
+    @import './utilities/_variables.scss';
+
+    .main {
+      display: flex;
+      flex-direction: column;
+
+      & .dark {
+        color: $color-white;
+        background-color: $color-black;
+      }
+    }
+  `,
 })
-export class AppComponent {
-  isDark = false;
+export class AppComponent implements OnInit {
+  isDark = true;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    this.updateBodyClass();
+  }
 
   toggleTheme() {
     this.isDark = !this.isDark;
+  }
+
+  updateBodyClass() {
+    const bodyClass = 'dark';
+    if (this.isDark) {
+      this.renderer.addClass(this.document.body, bodyClass);
+    } else {
+      this.renderer.removeClass(this.document.body, bodyClass);
+    }
   }
 }
