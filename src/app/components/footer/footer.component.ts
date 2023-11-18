e931e597aa356b40ca7c642fb94ccd4e96969417
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-11-18 09:32:49 -0300

    feat(footer.component.ts): add support for SvgIconComponent in the footer component to display an icon feat(footer.component.ts): add footer section with a link to the source website and an icon style(_base.scss): remove commented out code and update media query breakpoint to use em instead of px

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { SvgIconComponent } from '../ui/icons/svg-icon/svg-icon.component';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SvgIconComponent],
   template: `
-    <section class="container bottom-container">footer works!</section>
+    <footer class="container bottom-container footer">
+      <p class="footer-source__title">Source</p>
+
+      <a
+        href="https://en.wikipedia.org/wiki/Keyboard"
+        class="footer-source__link"
+      >
+        <span> https://en.wikipedia.org/wiki/Keyboard </span>
+
+        <app-svg-icon icon="link" height="12" width="12" />
+      </a>
+    </footer>
   `,
-  styles: ``,
+  styles: `
+    @import './utilities/mixins';
+    @import './utilities/variables';
+
+
+    .footer {
+      display: flex;
+      gap: .8rem;
+      flex-direction: column;
+      font-size: 1.4rem;
+      text-decoration-line: underline;
+      padding-top: 2.4rem;
+      border-top: 1px solid $color-lightest-gray;
+
+      @include media-query(tablet) {
+        flex-direction: row;
+        gap: 2rem;
+        margin-top: 1.9rem;
+      }
+
+      &-source__title {
+        color: $color-medium-gray;
+      }
+
+      &-source__link {
+        display: flex;
+        gap: .8rem;
+      }
+    }
+  `,
 })
 export class FooterComponent {}
